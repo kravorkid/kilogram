5f73312f983702cd4ba94e771dc5ffad66416ef7
kravorkid <matthieu.tabarin@gmail.com>
2019-02-19 18:18:44 +0100

    My first commit

---
+++ src/Kilogram.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Kilogram extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  
+  render() {
+    return (
+      <h2>Kilogram App</h2>
+    )
+  }
+}
+
+export default Kilogram;
